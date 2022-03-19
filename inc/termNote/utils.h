b997497e3a3060a20b3422e0d611ac838e6a6a59
Sebastian Muxel <sebastian@muxel.dev>
2022-03-19 01:35:45 +0100

    Remove utils from commands.c

---
+++ inc/termNote/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/*
+    Needs to be free()'d
+*/
+char* GetDataPath();
+char* GetFilePath();
+#endif
---
+++ src/utils.c
@@ -0,0 +1,59 @@
+#include "termNote/utils.h"
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+static const char FILE_NAME[] = "notes";
+static const char FILE_DIR[]  = "/termNote/";
+
+
+char* GetDataPath()
+{
+    const char* XDG_DATA_HOME = getenv("XDG_DATA_HOME");
+    char* dataPath = "";
+    if (XDG_DATA_HOME != NULL)
+    {
+        size_t xdg_len = strlen(XDG_DATA_HOME);
+
+        size_t maxLen = xdg_len + 1;
+        maxLen += strlen(FILE_DIR);
+        maxLen += strlen(FILE_NAME);
+
+        dataPath = (char*) malloc(maxLen + 1);
+        strcpy(dataPath, XDG_DATA_HOME);
+        if (XDG_DATA_HOME[xdg_len - 1] == '/')
+            strcat(dataPath, &FILE_DIR[1]);
+        else
+            strcat(dataPath, FILE_DIR);
+
+    } else
+    {
+        const char* HOME = getenv("HOME");
+        const char* subDir = "/.local/share";
+
+        size_t home_len = strlen(HOME);
+
+        size_t maxLen = home_len + 1;
+        maxLen += strlen(subDir);
+        maxLen += strlen(FILE_NAME);
+        maxLen += strlen(FILE_DIR);
+
+        dataPath = (char*) malloc(maxLen + 1);
+        strcpy(dataPath, HOME);
+        strcat(dataPath, subDir);
+        strcat(dataPath, FILE_DIR);
+    }
+    return dataPath;
+}
+
+char* GetFilePath()
+{
+    char* dataPath = GetDataPath();
+    char* filePath = malloc(strlen(dataPath) + strlen(FILE_NAME));
+
+    strcpy(filePath, dataPath);
+    free(dataPath);
+
+    strcat(filePath, FILE_NAME);
+    return filePath;
+}
